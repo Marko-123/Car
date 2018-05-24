36548c6f4a52480ce594a03f852cf0a311a8e22b
marko <kontakt@marko.rs>
2018-05-24 18:27:29 +0200

    school changes

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         
         Car myCar = new Car();
         System.out.println("Maksimalna brzina je: " + myCar.maxSpeed);
+        System.out.println(myCar.condition);
         
         Car companyCar = new Car(150, 30, 2000, false, "EFG-456", 'B', 3500, 60000);
         System.out.println("Maksimalna brzina je: " + companyCar.maxSpeed);
